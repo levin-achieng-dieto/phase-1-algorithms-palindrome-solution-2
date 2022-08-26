b6ab7d6071699220f4d1b24a311af785532b758f
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-27 01:40:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(i=0; i<word.length/2; i++){
+    const j = word.length -1 -i;
+    if(word[i] === word[j]){
+      return true
+    }
+    else{
+      return false
+    }
+  }
 }
 
 /* 
   Add your pseudocode here
+  create function that takes in a tring. if the 
+  forward indexes corresponding with the backward indexes
+  are equal, return true. else return false
 */
 
 /*
   Add written explanation of your solution here
+  create a function that takes in a string, and returns 
+  true if the foward indexes and the corresponding backward
+  backward indexes of a tring matches, else returns false
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +35,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("levin"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("dieto"));
 }
 
 module.exports = isPalindrome;
